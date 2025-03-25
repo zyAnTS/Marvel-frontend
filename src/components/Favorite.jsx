9b0f8bc2d6bea2ea208b192e8af4d7b07977e148
Yann Treuiller-Schlachter <yann.treuiller.schlachter@gmail.com>
2025-03-25 20:01:28 +0100

    Favorite test

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -10,81 +10,63 @@ const Favorite = ({ userToken, elem }) => {
   const [name, setName] = useState(elem.name);
   const [title, setTitle] = useState(elem.title);
   const [description, setDescription] = useState(elem.description);
-  const [favorite, setFavorite] = useState(true);
+
+  const handlePost = async (event) => {
+    event.stopPropagation();
+    elem.favorite = true;
+    if (userToken) {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/favorites/character",
+          {
+            thumbnail,
+            comics,
+            name,
+            title,
+            description,
+          },
+          {
+            headers: {
+              authorization: "Bearer " + userToken,
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        console.log(elem.comics);
+
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const handleDelete = async (event) => {
+    event.stopPropagation();
+    elem.favorite = false;
+    try {
+      const response = await axios.delete(
+        "https://site--marvel--mz8pkhlfl2x7.code.run/favorites/character/" +
+          elem._id
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   return (
     <>
       {elem.favorite ? (
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault();
-            try {
-              const response = await axios.delete(
-                "https://site--marvel--mz8pkhlfl2x7.code.run/favorites/character/" +
-                  elem._id
-              );
-              console.log(response.data);
-            } catch (error) {
-              console.log(error.response);
-            }
-          }}
-        >
-          <button
-            className="favorite-add"
-            onClick={(event) => {
-              event.stopPropagation();
-              elem.favorite = false;
-            }}
-          >
-            <i className="fa-solid fa-heart"></i>
-          </button>
-        </form>
+        <button className="favorite-add" onClick={handleDelete}>
+          <i className="fa-solid fa-heart"></i>
+        </button>
       ) : (
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault();
-
-            const formData = new FormData();
-            formData.append("thumbnail", thumbnail);
-            formData.append("comics", comics);
-            formData.append("name", name);
-            formData.append("title", title);
-            formData.append("description", description);
-            formData.append("favorite", favorite);
-
-            if (userToken) {
-              try {
-                const response = await axios.post(
-                  "http://localhost:3000/favorites/character",
-                  formData,
-                  {
-                    headers: {
-                      authorization: "Bearer " + userToken,
-                      "Content-Type": "multipart/form-data",
-                    },
-                  }
-                );
-                console.log(elem.comics);
-
-                console.log(response.data);
-              } catch (error) {
-                console.log(error.response);
-              }
-            } else {
-              navigate("/login");
-            }
-          }}
-        >
-          <button
-            className="favorite"
-            onClick={(event) => {
-              event.stopPropagation();
-              elem.favorite = true;
-            }}
-          >
-            <i className="fa-regular fa-heart"></i>
-          </button>
-        </form>
+        <button className="favorite" onClick={handlePost}>
+          <i className="fa-regular fa-heart"></i>
+        </button>
       )}
     </>
   );
